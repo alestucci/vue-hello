222bdb6b40bf0321fd9b82d00ebf37fea29d95e0
alestucci <alessandrotucci@gmail.com>
2022-03-10 15:51:10 +0100

    Job completed

---
+++ js/script.js
@@ -0,0 +1,10 @@
+const app = new Vue({
+	el: "#app",
+	data: {
+		title: "Not all those who wander are lost",
+		style: "text-align: center; font-size: 60px",
+		imgLink:
+			"https://www.collinsdictionary.com/images/full/newzealand_65046529_1000.jpg",
+		imgClassList: " center w-80",
+	},
+});
